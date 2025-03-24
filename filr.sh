f961606ed73298061261d8df4568daebe0eb8d10
Deepthichintha <deepthichintha2911@gmail.com>
2025-03-24 16:20:49 +0530

    secondcommit

---
+++ filr.sh
@@ -0,0 +1,39 @@
+what is operating system ?
+
+An Operating System (OS) is system software that manages computer hardware, software resources, and provides essential services for computer programs. It acts as an interface between the user and the computer hardware.
+
+Examples of Operating Systems:
+1.Windows (Windows 10, Windows 11)
+2.macOS (Monterey, Ventura)
+3.Linux (Ubuntu, Fedora)
+4.Android (Mobile OS)
+5.iOS (Apple’s mobile OS)
+ 
+what is linux ?
+linux is a free and open source operating system.
+
+Key Features of Linux:
+1.Open Source – Free to use, modify, and distribute.
+2.Multi-User & Multi-Tasking – Supports multiple users and tasks simultaneously.
+3.Security – Strong user permissions, firewalls, and encryption.
+4.Stability – Rarely crashes and performs well under heavy loads.
+5.light weight
+6.hard to hacking
+
+linux types:
+
+we have two types of linux operating systems there are
+     1.rpm (redhat package management)
+        Ex:
+           1.rpm
+           2.centos
+           3.fedora
+           4.amozon linux
+           5.open suse
+      2.debian based linux os
+         Ex:
+           1. debian
+           2. kali linux
+           3. ubuntu
+
+Devops stages?
